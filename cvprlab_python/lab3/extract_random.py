25d8b0fe921f00c7c5298b0aa9bf821de9393935
frankcholula <lu.phrank@gmail.com>
2024-10-11 12:48:52 +0100

    fixed lab 3.

--- cvprlab_python/lab3/extract_random.py
+++ cvprlab_python/lab3/extract_random.py
@@ -8,3 +8,22 @@ def extract_random(img):
     # computed from image 'img'
     # Note img is expected to be a normalized RGB image (colors range [0,1] not [0,255])
     return F
+
+
+def extract_rgb(img):
+    # Compute the average red value
+    red = img[:, :, 0]
+    average_red = np.mean(red)
+    
+    # Compute the average green value
+    green = img[:, :, 1]
+    average_green = np.mean(green)
+    
+    # Compute the average blue value
+    blue = img[:, :, 2]
+    average_blue = np.mean(blue)
+    
+    # Concatenate the average values to form the feature vector
+    F = np.array([average_red, average_green, average_blue])
+    
+    return F
